6692e395448e4056698b850b63cb7089633a1c1b
RJ Militante <134302968+rjgetvalify@users.noreply.github.com>
2023-06-01 16:44:32 -0500

    Hero component and interface

---
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent {
+
+  value = 1.23;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Big Boy'
+  };
+
+
+}
