143e4351a74df6fdce9ae4df9ddf8b9cc8425f98
RJ Militante <redmond007@gmail.com>
2023-06-08 15:54:44 -0500

    Message component and injectible service

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,27 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
 
-  // value = 1.23;
-
-  heroes = HEROES;
   selectedHero?: Hero;
 
-  // hero: Hero = {
-  //   id: 1,
-  //   name: 'Big Boy'
-  // };
+  heroes: Hero[] = [];
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    console.log(hero);
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
